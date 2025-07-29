39bd3248459fcff8b7885f88c66636a9afc274a8
Pedro Ezequiel <pedro_alonso_ezequiel@hotmail.com>
2025-07-28 21:04:40 -0300

    fix: Part3 validator regex

--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -21,7 +21,7 @@ const personSchema = new mongoose.Schema({
         minLength: 8,
         validate: {
             validator: function(v) {
-                return /\d{2}-\^[0-9]*$/.test(v) || /\d{3}-\^[0-9]*$/.test(v);
+                return /^\d{2,3}-\d{5,}$/.test(v);
             },
             message: props => `${props.value} is not a valid phone number!`
         },
