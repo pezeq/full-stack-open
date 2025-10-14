141904aa5a46fa5da8f4a3b25e3b915f681f7f9e
Pedro Ezequiel <pedro_alonso_ezequiel@hotmail.com>
2025-10-14 08:52:18 -0300

    chore: fix conflict artifact

--- part6/query-anecdotes/src/main.jsx
+++ part6/query-anecdotes/src/main.jsx
@@ -1,11 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-<<<<<<< HEAD
 import { NotificationContextProvider } from './NotificationContext';
-=======
-import { NotificationContextProvider } from './NotificationContext'
->>>>>>> d51c4876f9951d92b4914f0680ca6bfdcf84c4df
 
 const queryClient = new QueryClient();
 
