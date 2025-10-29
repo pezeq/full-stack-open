f085d3c821475f4e28c522af8e5fcdf0d45a6c2b
Pedro Ezequiel <pedro_alonso_ezequiel@hotmail.com>
2025-10-29 09:02:06 -0300

    feat: add logger and endpoint middleware

--- part7/bloglist-backend/app.js
+++ part7/bloglist-backend/app.js
@@ -2,11 +2,16 @@ const express = require('express');
 const app = express();
 const connectToMongoDB = require('./utils/db');
 const blogRouter = require('./routes/blogRouter');
+const middleware = require('./utils/middleware');
 
 connectToMongoDB();
 
 app.use(express.json());
+app.use(middleware.requestLogger);
 
 app.use('/api/blogs/', blogRouter);
+
+app.use(middleware.unknowEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
---
+++ part7/bloglist-backend/utils/middleware.js
@@ -0,0 +1,27 @@
+const logger = require('./logger');
+
+const requestLogger = (req, res, next) => {
+    logger.info('---');
+    logger.info('Method:\t', req.method);
+    logger.info('Path:\t', req.path);
+    logger.info('Body:\t', req.body);
+    logger.info('---');
+    next();
+};
+
+const unknowEndpoint = (req, res) => {
+    res.status(404).json({
+        error: '404: Not Found',
+        message: 'Unknown Endpoint',
+    });
+};
+
+const errorHandler = (err, req, res, next) => {
+    logger.error(err);
+};
+
+module.exports = {
+    requestLogger,
+    unknowEndpoint,
+    errorHandler,
+};
